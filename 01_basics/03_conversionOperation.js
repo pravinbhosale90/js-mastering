562822581f52a469810d3c3a7b60a80315edcd65
pravinbhosale90 <pravinrbhosale@yahoo.com>
2024-08-03 11:09:41 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -13,12 +13,41 @@ let valueInNumber= Number(score)
 
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 //1 => true, 0 => false
 //"" => false
 //"pravin" => true
 
 let age = 28
 let stringAge = String(age)
-console.log(stringAge)
-console.log(typeof stringAge)
+//console.log(stringAge)
+//console.log(typeof stringAge)
+
+
+//********* Operations ***********/
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue)
+
+let str ="hello"
+let str1 = " pravin"
+
+let str3 = str+str1
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 +2)
+// console.log(1 + 2 + "3")
+
+// console.log(true)
+// console.log(+true)
+// console.log("")
+// console.log(+"")
+
+let runs = 100
+console.log(runs++)
+console.log(++runs)
+
+//https://tc39.es/ecma262/#sec-type-conversion
