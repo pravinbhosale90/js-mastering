01071cdc791652f44157c56cd7038701df47d8bc
pravinbhosale90 <pravinrbhosale@yahoo.com>
2024-08-03 10:45:28 +0000

    data type and conversion of data type

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -2,14 +2,14 @@
 
 // alert(3+3) // we are using node js not browser
 
-let name = "Pravin"
+let username = "Pravin"
 let age = 30
 let isLoggedIn = false
 
 //number => 1 to power 53
 //bigint
 //string => ""
-//boolean => true/false
+//boolean => true/false 
 //null => standalone value
 //undefined => value not assigned
 //symbol => unique
@@ -17,6 +17,6 @@ let isLoggedIn = false
 
 //object
 
-console.log(typeof name)
+console.log(typeof username)
 console.log(typeof undefine) // undefined
 console.log(typeof null)  // object
