3982525f6652a4aa2ff184ba6ef05f99bec1bc9e
pravinbhosale90 <pravinrbhosale@yahoo.com>
2024-08-03 11:51:48 +0000

    heap and stack memory

--- 01_basics/data_type_summary.js
+++ 01_basics/data_type_summary.js
@@ -10,7 +10,7 @@ let userEmail
 
 const id = Symbol('123')
 const anotherId = Symbol('123')
-console.log(id==anotherId)
+// console.log(id==anotherId)
 
 //2. Reference data type (Non primitive)
 
@@ -26,6 +26,31 @@ const myFunction = function(){
     console.log("Hello Javascript")
 }
 
-console.log(typeof myFunction)
-console.log(typeof null)
-console.log(typeof person)
+// console.log(typeof myFunction)
+// console.log(typeof null)
+// console.log(typeof person)
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+//************MEMORY *************/
+
+// Stack (Primitive), Heap(Non-Primitive)
+
+let myTubeChannel ="pravinbhosaleyoutube"
+let anotherName = myTubeChannel
+anotherName = "chaiaurcode"
+// console.log(myTubeChannel)
+// console.log(anotherName)
+
+const user1 ={
+    name:"pravin",
+    email:'abc@google.com'
+}
+
+const user2 =user1
+
+user2.email = 'xyz@google.com'
+
+console.log(user1.email)
+console.log(user2.email)
+
