7b646ebe499bbea54d908749555e54e23062a1bb
pravinbhosale90 <pravinrbhosale@yahoo.com>
2024-08-03 10:29:06 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use stricts"; // treat all JS code as new version
+
+// alert(3+3) // we are using node js not browser
+
+let name = "Pravin"
+let age = 30
+let isLoggedIn = false
+
+//number => 1 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => value not assigned
+//symbol => unique
+
+
+//object
+
+console.log(typeof name)
+console.log(typeof undefine) // undefined
+console.log(typeof null)  // object
