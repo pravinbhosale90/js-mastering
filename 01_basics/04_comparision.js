2ec335f4ca354862b3a1b3203cd274f2b5ca5d41
pravinbhosale90 <pravinrbhosale@yahoo.com>
2024-08-03 11:18:41 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);// null converted 0 so it will return true
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === strict check (data type check)
+
+console.log("2" === 2);
